c9997f570352b82ed60999c4356dbd2dafb2d014
Kono <szwaj.jakub@gmail.com>
2022-08-04 19:01:15 +0200

    reading all from file

--- src/Memory.java
+++ src/Memory.java
@@ -1,24 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Memory {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args)  {
 
-        File words = new File("src/words/Words.txt");
-
-        Scanner s = new Scanner(words);
-        System.out.println(s.next());
-        System.out.println(s.next());
-        System.out.println(s.next());
-
-        s.close();
-
-        }
+        Memory memory = new Memory();
+        memory.getPlayerResponse();
+    }
     public void getPlayerResponse(){
 
+        Path path = Paths.get("src/words/Words.txt");
 
+        try (Stream<String> stream = Files.lines(path, StandardCharsets.UTF_8)) {
+            stream.forEach(System.out::println);
+        } catch (IOException ex) {
+            // Handle exception
+        }
     }
+
 }
