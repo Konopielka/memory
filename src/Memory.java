36c955177efe1dc375c5ae48760e4a5261a6e0be
Kono <szwaj.jakub@gmail.com>
2022-08-04 18:28:34 +0200

    reading from file

--- src/Memory.java
+++ src/Memory.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Memory {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File words = new File("src/words/Words.txt");
+
+        Scanner s = new Scanner(words);
+        System.out.println(s.next());
+        System.out.println(s.next());
+        System.out.println(s.next());
+
+        s.close();
+
+        }
+    public void getPlayerResponse(){
+
 
     }
 }
