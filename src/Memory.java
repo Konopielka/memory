d34a249ae2af6e2fa8c872b3441c48da598e9573
Kono <szwaj.jakub@gmail.com>
2022-08-08 15:11:35 +0200

    almost works

--- src/Memory.java
+++ src/Memory.java
@@ -15,13 +15,40 @@ public class Memory {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         Memory memory = new Memory();
         //memory.readFile();
         printBoard();
         System.out.println("");
         randomWord();
+
+        while (true){
+            System.out.println("press n for new game, q for quit");
+            String nq = scanner.nextLine();
+            if(nq.equals("q")){
+                System.out.println("end");
+                break;
+            }else if(nq.equals("n")){
+
+                randomWord();
+                for (int i = 0; i<2;i++){
+                    for (int j = 0; j<4;j++){
+                        board[i][j] = "X";
+                    }
+                }
+                printBoard();
+                checkInput(cards);
+                break;
+
+            }else{
+                System.out.println("press n for new game, q for quit");
+                continue;
+            }
+
+        }
+
     }
-        public static String randomWord (){
+        public static void randomWord (){
             List<String> listOfStrings = new ArrayList<String>();
             String filename = "C:\\Users\\Jakub Szwaj\\IdeaProjects\\memory\\memory\\src\\words\\Words.txt";
             try {
@@ -34,19 +61,19 @@ public class Memory {
 
             Random random = new Random();
 
-            int index = random.nextInt(array.length);
-            System.out.println(array[index]);
+//            int index = random.nextInt(array.length);
+//            System.out.println(array[index]);
 
 
-            /*for (int i =0; i<2; i++){
+            for (int i =0; i<2; i++){
                 for(int j = 0; j<4; j++){
-
+                    int index = random.nextInt(array.length);
                     cards[i][j] = array[index];
-                    Arrays.stream(array).toList().remove(index);
+                    //Arrays.stream(array).toList().remove(index);
                 }
-            }*/
+            }
 
-            return array[index];
+
         }
 
 //        public static void checkInput(String [][] cards){
@@ -90,13 +117,13 @@ public class Memory {
         for (int i = 0; i < 2; i++) {
 //            if (i == 0) {
 //                System.out.print("A ");
-            System.out.print("X");
+           //System.out.print("X");
 
             //System.out.print("B ");
 
             for (int j = 0; j < 4; j++) {
                 System.out.print(board[i][j]);
-                System.out.print("X");
+                //System.out.print("X");
             }
             System.out.println();
         }
@@ -107,9 +134,61 @@ public class Memory {
         while(true){
             if(!gameOver()){
                 System.out.println("row AB, column 1-4");
-                int rowColumnA1 = scanner.nextInt();
+                int rowA = scanner.nextInt();
+                int column1 = scanner.nextInt();
+
+                if (!board[rowA - 1][column1-1].equals("X")){
+                    System.out.println("Already entered");
+                    System.out.println();
+
+                    printBoard();
+                    continue;
+                }else {
+                    board[rowA-1][column1-1] = " " + cards[rowA-1][column1-1] + " ";
+                    printBoard();
+                }
+                System.out.println("rr");
+                int rowB = scanner.nextInt();
+                System.out.println("column1-4");
+                int column2 = scanner.nextInt();
+
+                if(!board[rowB-1][column2].equals("X")){
+                    System.out.println("already entered");
+                    board[rowA-1][column1-1] = "X";
+                    System.out.println();
+
+                            printBoard();
+
+                }else {
+                    board[rowB-1][column2-1] = " " + cards[rowB-1][column2-1]+ " ";
+
+                    if(board[rowA-1][column1-1].equals(board[rowB-1][column2-1])){
+                        printBoard();
+                        System.out.println("ok");
+                    }else {
+                        printBoard();
+                        System.out.println("false");
+                        board[rowA-1][column1-1] = "X";
+                        board[rowB-1][column2-1] = "X";
+                        printBoard();
+                    }
+                }
+
+            }else {
+                System.out.println("Game over");
+                break;
             }
         }
+    }
+    public static boolean gameOver(){
+        for (int i = 0; i<2; i++){
+            for (int j = 0; j<4;j++){
+                if(board[i][j].equals("X")){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
